1128b7ae32797b3cc46b9f4120190b8266c80277
Nadav <84288451+NotNadav@users.noreply.github.com>
2022-03-19 15:39:21 +0200

    Changed from run to execute
    done so it the command would work

--- commands/RerollGiveaway.js
+++ commands/RerollGiveaway.js
@@ -10,7 +10,7 @@ module.exports = {
         accessableby: "Admins",
         aliases: ["reroll"], // To add custom aliases just type ["alias1", "alias2"].
     },
-    run: async (client, message, args) => {
+    execute: async (client, message, args) => {
         if (!message.member.hasPermission('MANAGE_MESSAGES') && !message.member.roles.cache.some((r) => r.name === "Giveaways")) {
             return message.channel.send(':boom: You need to have the \`MANAGE_MESSAGES\` permission to reroll giveaways.');
         }
